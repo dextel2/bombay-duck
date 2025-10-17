35943338902c89cdf6aaef8c4ef47a60337ab065
PATEL PARTH <37260177+Dunexter@users.noreply.github.com>
2025-10-17 19:01:59 +0530

    Add Function to Fetch Daily Time Series from Alpha Vantage

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,3 +4,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 console.log("Starting NSE Upper Circuit Checker...");
+async function fetchDaily(symbol: string) {
+  const url = 'https://www.alphavantage.co/query';
+  const params = {
+    function: 'TIME_SERIES_DAILY_ADJUSTED',
+    symbol,
+    outputsize: 'compact',
+    apikey: process.env.ALPHA_VANTAGE_API_KEY,
+  };
+
+  const response = await axios.get(url, { params });
+  return response.data;
+}
